bf4d1339b9d70f13b0e97ae677d5114e47b92bb7
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-08-03 23:03:48 -0300

    :sparkles: feat: Lógica completamente reformulada, agora não usamos as funções disponiveis locais, mas sim as funções que hospedei. Além disso criei autenticador de acesso e usei o settings do django para poder pegar a API. Adicionei uma rota para login

--- investimentos/myapp/views.py
+++ investimentos/myapp/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .main import update_data_from_sheet, view_data_from_sheet
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+import requests
 
 def home(request):
     return render(request, 'myapp/index.html')
 
+@login_required
 def view_inv(request):
     return render(request, 'myapp/data.html')
 
+@login_required
 def get_sheet(request):
-    df = view_data_from_sheet()
-    data = df.to_dict(orient='list')  # Converte o DataFrame para um dicionário
+    headers = {
+        'api_key': settings.API_KEY
+    }
+    try:
+        response = requests.get('http://ec2-18-188-29-216.us-east-2.compute.amazonaws.com:8000/visualizar_dados_investimentos', headers=headers)
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        data = {'detail': 'Error fetching data', 'error': str(e)}
     return JsonResponse(data)
 
+@login_required
 def update_data(request):
-    selic, cdi, df = update_data_from_sheet()
-    data = {
-        'selic': selic,
-        'cdi': cdi,
-        'data_frame': df.to_dict(orient='list')  # Converte o DataFrame para um dicionário
+    headers = {
+        'api_key': settings.API_KEY
     }
+    try:
+        response = requests.post('http://ec2-18-188-29-216.us-east-2.compute.amazonaws.com:8000/atualizar_dados_investimentos', headers=headers)
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        data = {'detail': 'Error updating data', 'error': str(e)}
     return JsonResponse(data)
