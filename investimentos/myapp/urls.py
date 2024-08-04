2bd314959829d61f8186bfa44438cb445c52b76c
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-08-03 23:02:50 -0300

    :sparkles: feat: Adicionando a rota para autenticação

--- investimentos/myapp/urls.py
+++ investimentos/myapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('data/', views.update_data, name='data_view'),
     path('inv/', views.view_inv, name='view_inv'),
     path('get_sheet/', views.get_sheet, name='get_sheet'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
